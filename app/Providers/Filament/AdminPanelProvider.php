42ad35f2016fd4f545d656ba5fd4b79f22d7099a
AbeDevSaga <2000amandanm@gmail.com>
2025-03-18 15:15:16 +0300

    registering shield plugin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use function app_path;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -57,6 +58,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->plugins([
                 FilamentShieldPlugin::make(),
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
             ]);
+            
     }
 }
